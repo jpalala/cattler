7467b2ff8062f794113d047052ed13d8891ec711
Joe Palala <jose.palala@live.com>
2020-06-06 08:44:36 +0800

    06/2020 0844h: commit update

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 now=$(date +'%m/%Y %H%Mh')
-commitmessage="$now: commit update"
-git add . -A
-echo $commitmessage
-git commit -m="$commit_message"
+commit_message="$now: commit update"
+git add -A .
+echo $commit_message
+git commit -m "$commit_message"
 echo "now run:"
 echo "git push"
