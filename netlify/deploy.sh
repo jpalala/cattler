0b24628ab263fc06607532615ad8665a14396fdf
Joe Palala <jose.palala@live.com>
2020-06-06 08:15:40 +0800

    updated deploy script

--- netlify/deploy.sh
+++ netlify/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# deploys the dist to the master
+# run from parent root: ./netlify/deploy.sh
+# git pushes  the dist to the master
+now=$(date %Y/%m %r)
+cd dist/
+git checkout master
+git commit -am "$now: commit update"
+echo "now run `git push`"
 
